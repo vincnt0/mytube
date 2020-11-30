5d29f1be97c9c2e9a9cef9da92a0fe6e49ea5105
Vincent Wypior <vincent.wypior@st.oth-regensburg.de>
2020-11-30 22:51:03 +0100

    Integrated React

---
+++ webpack.config.js
@@ -0,0 +1,51 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const path = require('path');
+
+module.exports = {
+  mode: "production", // "production" | "development" | "none"
+  entry: "./src/index.js",
+  output: {
+    path: path.resolve(__dirname, "./build"), 
+    filename: "bundle.js",
+  },
+  devServer: {
+    contentBase: "./build"
+  },
+  target: "web",
+  resolve: {
+    modules: ["node_modules"],
+    extensions: [".js", ".json", ".jsx", ".css"],
+  },
+  module: {
+    // configuration regarding modules
+    rules: [
+      // rules for modules (configure loaders, parser options, etc.)
+     // we use babel-loader to load our jsx and tsx files
+      {
+        test: /\.jsx?$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader'
+        }
+      },
+
+      // css-loader to bundle all the css files into one file and style-loader to add all the styles  inside the style tag of the document
+      {
+        test: /\.css$/,
+        use: [
+          'style-loader', 
+          'css-loader'
+        ]
+      }
+    ],
+  },
+  plugins: [
+    new CopyWebpackPlugin({
+      patterns: [
+        { from: "public" }
+      ]
+    })
+  ],
+}
