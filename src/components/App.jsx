45c9ba7a6e83b8c3115f5854984100e5cc7e52f2
Vincent Wypior <vincent.wypior@st.oth-regensburg.de>
2020-12-01 11:51:32 +0100

    Created styled and routable options page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,49 @@
 import * as React from 'react';
+import {
+  BrowserRouter as Router,
+  Switch,
+  Route,
+  Link
+} from "react-router-dom";
 
-export default function App() {
+import "../styles/styles.css";
+
+export default function App({rootUrl}) {
   return (
-    <div>This is the App!</div>
+    <>
+      <header>
+        <img src="/icons/icon.png" class="icon"></img>
+        <h1>MyTube</h1>
+      </header>
+      <main>
+        
+        <Router>
+          <div class="main">
+            <nav class="nav-drawer">
+              <ul class="nav-list">
+                <li>
+                  <Link to={rootUrl + "/"}>Settings</Link>
+                </li>
+                <li>
+                  <Link to={rootUrl + "/about"}>About</Link>
+                </li>
+              </ul>
+            </nav>
+
+            {/* A <Switch> looks through its children <Route>s and
+                renders the first one that matches the current URL. */}
+            <Switch>
+              <Route path={rootUrl + "/about"}>
+                <div>About</div>
+              </Route>
+              <Route path={rootUrl + "/"}>
+                <div>Settings</div>
+              </Route>
+            </Switch>
+          </div>
+        </Router>
+
+      </main>
+    </>
   );
 }
--- src/index.js
+++ src/index.js
@@ -2,6 +2,4 @@ import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import App from './components/App';
 
-console.log("IM ALIVE")
-
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<App rootUrl="/options.html" />, document.getElementById('root'));
