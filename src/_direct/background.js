a7c445546e84aed46767a5c495ce2a794f3668e4
Vincent Wypior <vincent.wypior@st.oth-regensburg.de>
2020-12-09 13:20:24 +0100

    Hardcoded block, prepared for dynamic block

--- src/_direct/background.js
+++ src/_direct/background.js
@@ -7,7 +7,54 @@ chrome.runtime.onMessage.addListener((request, sender, sendResponse) => {
   }
 })
 
+var iconContextItem = {
+  "id": "mytube-icon",
+  "title": "Enable",
+  "contexts": ["page_action"]
+};
+chrome.contextMenus.create(iconContextItem);
 
+chrome.contextMenus.onClicked.addListener((info, tab) => {
+  if(info.id === iconContextItem.id){
+    //Enable here
+  }
+})
+
+const blocks_key = "mytube-blocks";
+var blocks = [];
+
+chrome.storage.sync.get([blocks_key], result => {
+  console.log("Initial blocks from storage: ", result[blocks_key]);
+  blocks = result[blocks_key];
+})
+
+chrome.storage.sync.onChanged.addListener((changes, namespace) => {
+  for(var key in changes){
+    if(key === blocks_key){
+      console.log("Received new blocks from storage: ", blocks);
+      blocks = changes[key].newValue;
+    }
+  }
+})
+
+function requestListener(page) {
+  var now = new Date(Date.now());
+  var cancel = true;
+  //Enable between 21 and 1
+  if(now.getHours() < 2 || now.getHours() >= 21){
+    cancel = false;
+  }
+
+  return {
+    cancel: cancel,
+  };
+}
+
+chrome.webRequest.onBeforeRequest.addListener(
+  requestListener,
+  { urls: ['https://www.youtube.com/*'] },
+  ['blocking']
+);
 
 /*
 var linkContextItem = {
