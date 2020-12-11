04e840c2a4ed0338c91c4fd5ca73ab0493010f8a
Vincent Wypior <vincent.wypior@st.oth-regensburg.de>
2020-12-11 03:31:17 +0100

    Added Clear Storage Button

--- src/components/settings/General.jsx
+++ src/components/settings/General.jsx
@@ -1,7 +1,19 @@
 import * as React from 'react';
 
+function clearStorage(){
+  if(confirm("WARNING: This will delete all settings and selections you have made within this extension. Are you sure you want to proceed?")){
+    chrome.storage?.sync.clear();
+  }
+}
+
 export default function General(){
+
   return <>
-    No general settings available yet!
+  <div>
+    <button onClick={clearStorage}>
+      Clear Extension Storage
+    </button>
+    Warning: Resets all other extensions settings and preferences!
+  </div>
   </>
 }
